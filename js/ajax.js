0a1367cd7733cc832d8e60be95319e7c3290b7e1
creatizer.mischenko <mischenko@creatizer.ru>
2017-11-22 21:17:57 +0500

    start ajax

---
+++ js/ajax.js
@@ -0,0 +1,25 @@
+let checkForm = true;
+
+$('#order-form').on('submit', function(e) {
+            e.preventDefault();
+            console.log($('.form-block').children('[type]', 'text')).foreach(function() {
+
+            }) {
+
+                //if ($('.form-block').foreach(children($('[type]', 'text')[0]).value) == '') then { checkForm = false }
+
+                // if ($('.order__name')[0].value == '') { checkForm = false }
+                // if ($('.order__tel')[0].value == '') { checkForm = false }
+                // if ($('.order__name')[0].value == '') { checkForm = false }
+                // if ($('.order__name')[0].value == '') { checkForm = false }
+                // if ($('.order__name')[0].value == '') { checkForm = false }
+                // if ($('.order__name')[0].value == '') { checkForm = false }
+                // if ($('.order__name')[0].value == '') { checkForm = false }
+                // if ($('.order__name')[0].value == '') { checkForm = false }
+                // if ($('.order__name')[0].value == '') { checkForm = false }
+                // if ($('.order__name')[0].value == '') { checkForm = false }
+
+                // console.log($('.order__name')[0]);
+                // console.log(checkForm);
+                //console.log($(this).serialize());
+            })
